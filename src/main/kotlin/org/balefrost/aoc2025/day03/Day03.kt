f2cebe8b3840a0a072e98850cc9f4ba2c43d210f
Dan Yankowsky <bytecrafter@gmail.com>
2025-12-02 22:10:14 -0800

    Day03 part1 part2

---
+++ src/main/kotlin/org/balefrost/aoc2025/day03/Day03.kt
@@ -0,0 +1,42 @@
+package org.balefrost.aoc2025.day03
+
+import org.balefrost.aoc2025.readInputLines
+import kotlin.math.pow
+
+fun maxJoltage(digits: List<Int>, remaining: Int): Long {
+    data class Key(val start: Int, val remaining: Int)
+    val cache = mutableMapOf<Key, Long>()
+
+    fun helper(start: Int, remaining: Int): Long {
+        return cache.getOrPut(Key(start, remaining)) {
+            if (remaining == 0) { return 0L }
+            check(digits.size - start >= remaining)
+            for( d in 9 downTo 0) {
+                val i = start + digits.subList(start, digits.size).indexOf(d)
+                if (i >= start && i <= digits.size - remaining) {
+                    val y = helper(i + 1, remaining - 1)
+                    return@getOrPut 10.0.pow((remaining - 1).toDouble()).toLong() * d + y
+                }
+            }
+            error("Could not find any digits in digits")
+        }
+    }
+
+    return helper(0, remaining)
+}
+
+object Day03Part01 {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val lines = readInputLines("inputs/day03.txt")
+        println(lines.sumOf { maxJoltage(it.map { it.digitToInt() }, 2) })
+    }
+}
+
+object Day03Part02 {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val lines = readInputLines("inputs/day03.txt")
+        println(lines.sumOf { maxJoltage(it.map { it.digitToInt() }, 12) })
+    }
+}
